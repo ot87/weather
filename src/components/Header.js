3c5e4681e1a5ae010ae21623d5294c6db5baf1d4
ot87 <oleksandr.tsyrkul@gmail.com>
2021-12-18 18:04:14 +0200

    Add styles to Header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,21 @@ import styled from 'styled-components';
 
 const StyledHeader = styled.header`
   height: 10%;
+  padding: 10px;
+  margin: 0 auto;
 `;
 
-const Header = () => {
-  return <StyledHeader></StyledHeader>;
-};
+const StyledHeadline = styled.h1`
+  font-size: 48px;
+  font-weight: 300;
+  letter-spacing: 2px;
+  line-height: 1;
+`;
+
+const Header = () => (
+  <StyledHeader>
+    <StyledHeadline>WEATHER</StyledHeadline>
+  </StyledHeader>
+);
 
 export default Header;
