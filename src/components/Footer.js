20f93ae75176a65594bb2144216a08e6ca23fb9e
ot87 <oleksandr.tsyrkul@gmail.com>
2021-12-18 18:55:34 +0200

    Add styles to Footer Component with the link to Open Meteo API

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,25 @@
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
   height: 10%;
+  font-size: 20px;
+  padding: 10px;
 `;
 
-const Footer = () => {
-  return <StyledFooter></StyledFooter>;
-};
+const Footer = () => (
+  <StyledFooter>
+    <span>API by</span>
+    <span>
+      <a target='_blank' rel='noreferrer' href='https://open-meteo.com/'>
+        Weather data by Open-Meteo.com
+      </a>
+    </span>
+  </StyledFooter>
+);
 
 export default Footer;
