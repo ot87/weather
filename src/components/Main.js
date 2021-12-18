b80d10f8dee98ba0f26c9ab4713602777d287c4b
ot87 <oleksandr.tsyrkul@gmail.com>
2021-12-18 19:29:47 +0200

    Add flex styles to Main component.

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 const StyledMain = styled.main`
   padding: 10px;
   height: 80%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
 `;
 
 const Main = () => {
